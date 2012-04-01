cefb366ab24b7638accde07258de17f324e78107
travp624 <stygianstudiosdevs@gmail.com>
2012-04-01 04:29:24 -0500

    bump ver. / change build id up

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,7 +1,7 @@
 # Version information used on all builds
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IML74K BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76D BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-29
+CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-29.29
 
 ifneq ($(TARGET_PRODUCT),BlackICE_p999)
     ifeq ($(TARGET_PRODUCT),BlackICE_p990)
