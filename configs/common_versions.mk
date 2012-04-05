72a93e7b1a80f9b9036bc24d14f9e7286040a519
travp624 <stygianstudiosdevs@gmail.com>
2012-04-05 09:04:10 -0500

    add option for camera sounds disable (1/3) [sethyx]

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,7 +1,7 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76D BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-29.29
+CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-30.30
 
 ifneq ($(TARGET_PRODUCT),BlackICE_p999)
     ifeq ($(TARGET_PRODUCT),BlackICE_p990)
@@ -26,4 +26,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.rom=$(TARGET_PRODUCT) \
     ro.goo.version=1
 
-
+# Camera shutter sound property
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.camera-sound=1
