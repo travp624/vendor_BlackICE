ad20b7ea937a322bb838b1cc4c23e5287ec0b129
travp624 <stygianstudiosdevs@gmail.com>
2012-04-29 13:39:16 -0500

    bump to next build if made ready

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,7 +1,7 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-34.69
+CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-34.70
 
 ifneq ($(TARGET_PRODUCT),BlackICE_p999)
     ifeq ($(TARGET_PRODUCT),BlackICE_p990)
@@ -18,14 +18,13 @@ endif
 
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.modversion=$(CURRENT_VERSION) \
-    ro.rommanager.developerid=travp624 \
+    ro.rommanager.developerid=travp624
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=travp624 \
     ro.goo.rom=$(TARGET_PRODUCT) \
-    ro.goo.version=1
+    ro.goo.version=2
 
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
