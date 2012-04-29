e53afef9b9929173c857759c03ae677146c915c4
travp624 <stygianstudiosdevs@gmail.com>
2012-04-29 03:23:08 -0500

    update kang for fixes

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,7 +1,7 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
-CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-34.43
+CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-34.69
 
 ifneq ($(TARGET_PRODUCT),BlackICE_p999)
     ifeq ($(TARGET_PRODUCT),BlackICE_p990)
