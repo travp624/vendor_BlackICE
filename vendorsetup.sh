9967dd36e99426743b31c59b8876b1cca259ef0c
travp624 <stygianstudiosdevs@gmail.com>
2012-04-28 23:14:26 -0500

    configure vendorsetup for buildbot. add vendorsetup blank to hold alternate builds

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,18 +1,5 @@
 add_lunch_combo BlackICE_maguro-userdebug
 add_lunch_combo BlackICE_toro-userdebug
-add_lunch_combo BlackICE_crespo-userdebug
-add_lunch_combo BlackICE_crespo4g-userdebug
-add_lunch_combo BlackICE_fascinatemtd-userdebug
-add_lunch_combo BlackICE_inc-userdebug
-# add_lunch_combo BlackICE_otter-userdedug
-add_lunch_combo BlackICE_p4-userdebug
-add_lunch_combo BlackICE_p4vzw-userdebug
-add_lunch_combo BlackICE_p4wifi-userdebug
 add_lunch_combo BlackICE_shadow-userdebug
-add_lunch_combo BlackICE_sholes-userdebug
 add_lunch_combo BlackICE_stingray-userdebug
-add_lunch_combo BlackICE_supersonic-userdebug
-add_lunch_combo BlackICE_tenderloin-userdebug
-# add_lunch_combo BlackICE_vzwtab-userdebug
-add_lunch_combo BlackICE_vivow-userdebug
 add_lunch_combo BlackICE_wingray-userdebug
