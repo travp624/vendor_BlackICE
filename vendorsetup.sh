150cf918f4d3dc1491c484071c8a4864dac8dffe
travp624 <stygianstudiosdevs@gmail.com>
2012-05-04 23:33:46 -0500

    add crespo and crespo4g to build

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,6 +1,8 @@
 add_lunch_combo BlackICE_maguro-userdebug
 add_lunch_combo BlackICE_toro-userdebug
 add_lunch_combo BlackICE_toroplus-userdebug
+add_lunch_combo BlackICE_crespo-userdebug
+add_lunch_combo BlackICE_crespo4g-userdebug
 add_lunch_combo BlackICE_otter-userdebug
 add_lunch_combo BlackICE_shadow-userdebug
 add_lunch_combo BlackICE_stingray-userdebug
