41ac5943c36cdb2dd6bfa636fb1527c39aca6e9c
travp624 <stygianstudiosdevs@gmail.com>
2012-05-08 13:48:58 -0500

    revert "rename modversion (2/2) Changed modversion to aokp.version and cleaned up p99x defines."
    Signed-off-by: Jonathan Steadman <xxwhitehawk@gmail.com>
    Change-Id: I90bdde984241394dfd3d4d55bce81dabf927b14a

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -1,10 +1,24 @@
 # Version information used on all builds
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_DISPLAY_ID=IMM76I BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
+CURRENT_VERSION=$(TARGET_PRODUCT)-Kangorade-34.80
+
+ifneq ($(TARGET_PRODUCT),BlackICE_p999)
+    ifeq ($(TARGET_PRODUCT),BlackICE_p990)
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.cm.version=9.0.0-$(CURRENT_VERSION)
+    else
+        PRODUCT_PROPERTY_OVERRIDES += \
+            ro.modversion=$(CURRENT_VERSION)
+    endif
+else
+    PRODUCT_PROPERTY_OVERRIDES += \
+        ro.cm.version=9.0.0-$(CURRENT_VERSION)
+endif
+
 # Rom Manager properties
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.rommanager.developerid=travp624 \
-    ro.blackice.version=$(TARGET_PRODUCT)-Kangorade-34.80
+    ro.rommanager.developerid=travp624 
 
 # Goo updater app
 PRODUCT_PROPERTY_OVERRIDES += \
