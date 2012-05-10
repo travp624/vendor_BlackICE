e3b05094fba35cb028c7290744bdb39ffa1f9dbd
travp624 <stygianstudiosdevs@gmail.com>
2012-05-09 23:26:18 -0500

    freaking typos.

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -6,4 +6,4 @@ PRODUCT_PACKAGES += \
        com.tmobile.themes
 
 PRODUCT_COPY_FILES += \
-       vendor/BlackICE/prebuilt/commom/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/BlackICE/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
