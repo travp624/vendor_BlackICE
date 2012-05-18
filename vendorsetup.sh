3eb533da2648704893554ef83ce3ac02a19cce38
travp624 <stygianstudiosdevs@gmail.com>
2012-05-18 11:04:00 -0500

    initial vendor setup for motoics branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,2 @@
-add_lunch_combo BlackICE_maguro-userdebug
-add_lunch_combo BlackICE_toro-userdebug
-add_lunch_combo BlackICE_toroplus-userdebug
-add_lunch_combo BlackICE_crespo-userdebug
-add_lunch_combo BlackICE_crespo4g-userdebug
-add_lunch_combo BlackICE_otter-userdebug
-add_lunch_combo BlackICE_stingray-userdebug
-add_lunch_combo BlackICE_tenderloin-userdebug
-add_lunch_combo BlackICE_wingray-userdebug
+add_lunch_combo BlackICE_shadow-userdebug
+add_lunch_combo BlackICE_spyder-userdebug
