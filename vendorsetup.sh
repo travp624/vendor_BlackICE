2253350b8c12bb6c70619b6e813195e08427b2b4
travp624 <stygianstudiosdevs@gmail.com>
2012-05-18 09:25:27 -0500

    remove shadow from bot builds as it will need separate frameworks branch now to build.

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,6 @@ add_lunch_combo BlackICE_toroplus-userdebug
 add_lunch_combo BlackICE_crespo-userdebug
 add_lunch_combo BlackICE_crespo4g-userdebug
 add_lunch_combo BlackICE_otter-userdebug
-add_lunch_combo BlackICE_shadow-userdebug
 add_lunch_combo BlackICE_stingray-userdebug
 add_lunch_combo BlackICE_tenderloin-userdebug
 add_lunch_combo BlackICE_wingray-userdebug
