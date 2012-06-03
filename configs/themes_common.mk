c2522cf6f4bb7fee65ef210c78e41d1a2b292e94
travp624 <travp624@gmail.com>
2012-06-03 14:53:54 -0500

    add ICEpurple to the theme engine

--- configs/themes_common.mk
+++ configs/themes_common.mk
@@ -1,12 +1,13 @@
 # T-Mobile theme engine
 PRODUCT_PACKAGES += \
-       ICEBlu \
-       ICEGreen \
-       ICEOrange \
-       ICERed \
-       ThemeManager \
-       ThemeChooser \
-       com.tmobile.themes
+    ICEBlu \
+    ICEGreen \
+    ICEOrange \
+    ICEPurple \
+    ICERed \
+    ThemeManager \
+    ThemeChooser \
+    com.tmobile.themes
 
 PRODUCT_COPY_FILES += \
-       vendor/BlackICE/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+    vendor/BlackICE/prebuilt/common/etc/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
