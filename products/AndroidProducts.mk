e97425752dd8ed255b9d6eb2da73123b443c1135
travp624 <travp624@gmail.com>
2012-07-02 00:07:56 -0500

    cleanup and slim down moto branch

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,20 +1,3 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/crespo.mk \
-    $(LOCAL_DIR)/crespo4g.mk \
-    $(LOCAL_DIR)/fascinatemtd.mk \
-    $(LOCAL_DIR)/inc.mk \
-    $(LOCAL_DIR)/maguro.mk \
-    $(LOCAL_DIR)/otter.mk \
-    $(LOCAL_DIR)/p4.mk \
-    $(LOCAL_DIR)/p4vzw.mk \
-    $(LOCAL_DIR)/p4wifi.mk \
     $(LOCAL_DIR)/shadow.mk \
-    $(LOCAL_DIR)/sholes.mk \
     $(LOCAL_DIR)/spyder.mk \
-    $(LOCAL_DIR)/stingray.mk \
-    $(LOCAL_DIR)/supersonic.mk \
-    $(LOCAL_DIR)/tenderloin.mk \
-    $(LOCAL_DIR)/toro.mk \
-    $(LOCAL_DIR)/toroplus.mk \
-    $(LOCAL_DIR)/vivow.mk \
-    $(LOCAL_DIR)/wingray.mk \
